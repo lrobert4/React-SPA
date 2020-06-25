a3eb5cbcfa58c96ee63af21b01b48e17ab2a9255
leer <ladesigns90@gmail.com>
2020-06-25 12:38:47 -0400

    Added index.js html markup and linkedin boostrap css for styles.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 // Import React
 import React, { Component } from 'react';
-
-import 'bootstrap/dist/css/bootstrap.css';
+import Home from './Home';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Meeting Log</h1>
-      </div>
+      <Home />
     );
   }
 }
