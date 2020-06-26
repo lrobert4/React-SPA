31126f925e7efc45f4bff88e736221a4897915af
leer <ladesigns90@gmail.com>
2020-06-26 00:52:43 -0400

    Added css styles and added props as well as conditional components for certain elements to display to respective state.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 // Import React
 import React, { Component } from 'react';
 import Home from './Home';
+import Welcome from './Welcome';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      user: 'Raybo'
+    }
+  }
+
   render() {
     return (
-      <Home />
+      <div>
+        {this.state.user && <Welcome user={this.state.user}/>}
+        
+        <Home user={this.state.user}/>
+
+      </div>
+      
+
     );
   }
 }
--- src/Home.js
+++ src/Home.js
@@ -1,32 +1,54 @@
 import React, { Component } from 'react';
 
-class Home extends React.Component {
+class Home extends Component {
     render() {
+
+        const {user} = this.props;
+
+        const biggerLead = {
+            fontSize: 1.4 + 'em',
+            fontWeight: 200
+        };
+
         return(
             <div className="container text-center">
                 <div className="row justify-content-center">
+                    
                     <div className="col-10 col-md-10 col-lg-8 col-xl-7">
-                    <div className="display-4 text-primary mt-3 mb-2">
+                    <div className="display-4 text-primary mt-3 mb-2" 
+                    style={{fontSize: 2.8 + 'em'}}>
                         Meeting Log
                     </div>
-                    <p className="lead">
+                    <p className="lead" style={biggerLead}>
                         This simple app creates meetings, allows people to check
                         in, and picks random users to award giveaways. It's a
                         good example of a Single Page Application which includes
                         connection to a database and routing. It's a practical
-                        way to learn <a href="https://reactjs.org/">React</a> 
+                        way to learn <a href="https://reactjs.org/">React</a>{' '} 
                         with <a href="https://firebase.google.com">Firebase</a>.
                     </p>
 
-                    <a href="/register" className="btn btn-outline-primary mr-2">
-                        Register
-                    </a>
-                    <a href="/login" className="btn btn-outline-primary mr-2">
-                        Log In
-                    </a>
-                    <a href="/meetings" className="btn btn-primary">
+                    {/* javascript technique: switch */}
+
+                    {user == null && (
+                        <span>
+                            <a href="/register" className="btn btn-outline-primary mr-2">
+                                Register
+                            </a>
+                            <a href="/login" className="btn btn-outline-primary mr-2">
+                                Log In
+                            </a>
+                        </span>
+                    )}
+
+                    {/* switch - If the statement of the left evauates to TRUE, it executes code to the right */}
+
+                    {user && (
+                        <a href="/meetings" className="btn btn-primary">
                         Meetings
-                    </a>
+                        </a>
+                    )}
+                    
                     </div> {/*  columns */}
                 </div>
             </div>
